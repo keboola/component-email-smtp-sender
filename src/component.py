2114690549d8c3bc7e4e99d035ea4a9086cc13fa
samuelhavelka <samuel.havelka@keboola.com>
2024-02-29 15:10:08 +0100

    0365 + basic options variant

--- src/component.py
+++ src/component.py
@@ -60,21 +60,34 @@ class Component(ComponentBase):
         self.init_client()
         in_tables = self.get_input_tables_definitions()
         in_files_by_name = self.get_input_file_definitions_grouped_by_name()
-        in_table_path = in_tables[0].full_path
+        email_data_table_name = self.cfg.advanced_options.email_data_table_name
+        try:
+            email_data_table_path = next(in_table.full_path for in_table in in_tables
+                                         if in_table.name == email_data_table_name)
+        except StopIteration:
+            email_data_table_path = None
         self.plaintext_template_path, self.html_template_path = \
             self._extract_template_files_full_paths(in_files_by_name)
 
-        attachments_paths_by_filename = {
+        table_attachments_paths_by_filename = {
+            in_table.name: in_table.full_path
+            for in_table in in_tables
+            if in_table.name != email_data_table_name
+        }
+        file_attachments_paths_by_filename = {
             name: files[0].full_path
             for name, files in in_files_by_name.items()
             if files[0].full_path not in [self.plaintext_template_path, self.html_template_path]}
+
+        attachments_paths_by_filename = {**table_attachments_paths_by_filename, **file_attachments_paths_by_filename}
 
         results_table = self.create_out_table_definition('results.csv', write_always=True)
         with open(results_table.full_path, 'w', newline='') as output_file:
             self._results_writer = csv.DictWriter(output_file, fieldnames=RESULT_TABLE_COLUMNS)
             self._results_writer.writeheader()
             self._results_writer.errors = False
-            self.send_emails(in_table_path, attachments_paths_by_filename=attachments_paths_by_filename)
+            self.send_emails(email_data_table_path=email_data_table_path,
+                             attachments_paths_by_filename=attachments_paths_by_filename)
         self.write_manifest(results_table)
 
         if self._results_writer.errors:
@@ -87,58 +100,78 @@ class Component(ComponentBase):
     def init_client(self, connection_config: Union[ConnectionConfig, None] = None) -> None:
         if connection_config is None:
             connection_config = self.cfg.connection_config
-        proxy_server_config = connection_config.proxy_server_config
+        proxy_server_config = connection_config.creds_config.proxy_server_config
+        oauth_config = connection_config.oauth_config
+        creds_config = connection_config.creds_config
         self._client = SMTPClient(
-            sender_email_address=connection_config.sender_email_address,
-            password=connection_config.pswd_sender_password,
-            server_host=connection_config.server_host,
-            server_port=connection_config.server_port,
-            connection_protocol=connection_config.connection_protocol,
+            use_oauth=connection_config.use_oauth,
+            sender_email_address=creds_config.sender_email_address,
+            password=creds_config.pswd_sender_password,
+            server_host=creds_config.server_host,
+            server_port=creds_config.server_port,
+            connection_protocol=creds_config.connection_protocol,
             proxy_server_host=proxy_server_config.proxy_server_host,
             proxy_server_port=proxy_server_config.proxy_server_port,
             proxy_server_username=proxy_server_config.proxy_server_username,
-            proxy_server_password=proxy_server_config.pswd_proxy_server_password)
+            proxy_server_password=proxy_server_config.pswd_proxy_server_password,
+            tenant_id=oauth_config.tenant_id,
+            client_id=oauth_config.client_id,
+            client_secret=oauth_config.pswd_client_secret)
         self._client.init_smtp_server()
 
-    def send_emails(self, in_table_path: str, attachments_paths_by_filename: Dict[str, str]) -> None:
+    def send_emails(self, attachments_paths_by_filename: Dict[str, str],
+                    email_data_table_path: Union[str, None] = None) -> None:
         continue_on_error = self.cfg.continue_on_error
         dry_run = self.cfg.dry_run
-        subject_config = self.cfg.subject_config
-        message_body_config = self.cfg.message_body_config
-        attachments_config = self.cfg.attachments_config
+        use_advanced_options = self.cfg.use_advanced_options
+        basic_options = self.cfg.basic_options
+        subject_config = self.cfg.advanced_options.subject_config
+        message_body_config = self.cfg.advanced_options.message_body_config
+        attachments_config = self.cfg.advanced_options.attachments_config
         use_html_template = message_body_config.use_html_template
 
-        with open(in_table_path) as in_table:
-            reader = csv.DictReader(in_table)
-            columns = set(reader.fieldnames)
+        rendered_subject = basic_options.subject
+        rendered_plaintext_message = basic_options.message_body
+        rendered_html_message = None
+        custom_attachments_paths_by_filename = attachments_paths_by_filename
 
-            subject_column = None
-            if subject_config.subject_source == 'from_table':
-                subject_column = subject_config.subject_column
-            else:
-                subject_template_text = subject_config.subject_template_definition
-                self._validate_template_text(subject_template_text, columns)
+        if email_data_table_path is not None:
+            with open(email_data_table_path) as in_table:
+                reader = csv.DictReader(in_table)
+                columns = set(reader.fieldnames)
 
-            html_template_column = None
-            if message_body_config.message_body_source == 'from_table':
-                plaintext_template_column = message_body_config.plaintext_template_column
-                if use_html_template:
-                    html_template_column = message_body_config.html_template_column
-            else:
-                plaintext_template_column = None
-                plaintext_template_text = self._read_template_text()
-                self._validate_template_text(plaintext_template_text, columns)
-                if use_html_template:
-                    html_template_text = self._read_template_text(plaintext=False)
-                    self._validate_template_text(html_template_text, columns)
+                subject_column = None
+                if subject_config.subject_source == 'from_table':
+                    subject_column = subject_config.subject_column
+                else:
+                    subject_template_text = subject_config.subject_template_definition
+                    self._validate_template_text(subject_template_text, columns)
 
-            all_attachments = attachments_config.attachments_source == 'all_input_files'
-            if not all_attachments:
-                attachments_column = attachments_config.attachments_column
+                html_template_column = None
+                if message_body_config.message_body_source == 'from_table':
+                    plaintext_template_column = message_body_config.plaintext_template_column
+                    if use_html_template:
+                        html_template_column = message_body_config.html_template_column
+                else:
+                    plaintext_template_column = None
+                    plaintext_template_text = self._read_template_text()
+                    self._validate_template_text(plaintext_template_text, columns)
+                    if use_html_template:
+                        html_template_text = self._read_template_text(plaintext=False)
+                        self._validate_template_text(html_template_text, columns)
 
-            for row in reader:
-                recipient_email_address = row[self.cfg.recipient_email_address_column]
-                try:
+                all_attachments = attachments_config.attachments_source == 'all_input_files'
+                if not all_attachments:
+                    attachments_column = attachments_config.attachments_column
+        else:
+            reader = iter(self.cfg.basic_options.recipient_email_addresses.split(','))
+
+        for row in reader:
+            try:
+                if not use_advanced_options:
+                    recipient_email_address = row
+                else:
+                    recipient_email_address = row[self.cfg.advanced_options.recipient_email_address_column]
                     if subject_column is not None:
                         subject_template_text = row[subject_column]
                         self._validate_template_text(subject_template_text, columns)
@@ -169,55 +202,56 @@ class Component(ComponentBase):
                             for attachment_filename in json.loads(row[attachments_column])
                         }
 
-                    email_ = self._client.build_email(
-                        recipient_email_address=recipient_email_address,
-                        subject=rendered_subject,
-                        attachments_paths_by_filename=custom_attachments_paths_by_filename,
-                        rendered_plaintext_message=rendered_plaintext_message,
-                        rendered_html_message=rendered_html_message)
+                email_ = self._client.build_email(
+                    recipient_email_address=recipient_email_address,
+                    subject=rendered_subject,
+                    attachments_paths_by_filename=custom_attachments_paths_by_filename,
+                    rendered_plaintext_message=rendered_plaintext_message,
+                    rendered_html_message=rendered_html_message)
 
-                    status = 'OK'
-                    error_message = ''
-                    if not dry_run:
-                        try:
-                            logging.info(
-                                f"Sending email with subject: `{email_['Subject']}`"
-                                f" from `{email_['From']}` to `{email_['To']}`")
-                            self._client.send_email(email_)
-                        except Exception as e:
-                            error_message = str(e)
-                            status = 'ERROR'
-                            self._results_writer.errors = True
+                status = 'OK'
+                error_message = ''
+                if not dry_run:
+                    try:
+                        logging.info(
+                            f"Sending email with subject: `{email_['Subject']}`"
+                            f" from `{email_['From']}` to `{email_['To']}`")
+                        self._client.send_email(email_, message=rendered_plaintext_message,
+                                                attachments_paths=attachments_paths_by_filename.values())
+                    except Exception as e:
+                        error_message = str(e)
+                        status = 'ERROR'
+                        self._results_writer.errors = True
 
-                    rendered_html_message_writable = ''
-                    if rendered_html_message is not None:
-                        rendered_html_message_writable = rendered_html_message
+                rendered_html_message_writable = ''
+                if rendered_html_message is not None:
+                    rendered_html_message_writable = rendered_html_message
 
-                    self._results_writer.writerow(dict(
-                        status=status,
-                        recipient_email_address=email_['To'],
-                        sender_email_address=email_['From'],
-                        subject=email_['Subject'],
-                        plaintext_message_body=rendered_plaintext_message,
-                        html_message_body=rendered_html_message_writable,
-                        attachment_filenames=json.dumps(list(custom_attachments_paths_by_filename)),
-                        error_message=error_message))
-                    time.sleep(SLEEP_INTERVAL)
-                except Exception as e:
-                    if not continue_on_error:
-                        raise UserException(
-                            'Error occurred, when trying to send an email. Please validate your configuration.')
-                    self._results_writer.writerow({
-                        **general_error_row,
-                        'sender_email_address': self._client.sender_email_address,
-                        'recipient_email_address': recipient_email_address,
-                        'error_message': str(e)})
-                    self._results_writer.errors = True
+                self._results_writer.writerow(dict(
+                    status=status,
+                    recipient_email_address=email_['To'],
+                    sender_email_address=email_['From'],
+                    subject=email_['Subject'],
+                    plaintext_message_body=rendered_plaintext_message,
+                    html_message_body=rendered_html_message_writable,
+                    attachment_filenames=json.dumps(list(custom_attachments_paths_by_filename)),
+                    error_message=error_message))
+                time.sleep(SLEEP_INTERVAL)
+            except Exception as e:
+                if not continue_on_error:
+                    raise UserException(
+                        'Error occurred, when trying to send an email. Please validate your configuration.')
+                self._results_writer.writerow({
+                    **general_error_row,
+                    'sender_email_address': self._client.sender_email_address,
+                    'recipient_email_address': recipient_email_address,
+                    'error_message': str(e)})
+                self._results_writer.errors = True
 
     def _extract_template_files_full_paths(
             self, in_files_by_name: Dict[str, List[FileDefinition]]) -> Tuple[Union[str, None], Union[str, None]]:
         """Extracts full paths for template files if they are provided"""
-        msg_body_config = self.cfg.message_body_config
+        msg_body_config = self.cfg.advanced_options.message_body_config
         plaintext_template_path = None
         html_template_path = None
         if msg_body_config.message_body_source == 'from_template_file':
@@ -252,7 +286,7 @@ class Component(ComponentBase):
         attachments_filenames = set()
         with open(in_table_path) as in_table:
             reader = csv.DictReader(in_table)
-            attachments_column = self.cfg.attachments_config.attachments_column
+            attachments_column = self.cfg.advanced_options.attachments_config.attachments_column
             for row in reader:
                 for attachment_filename in json.loads(row[attachments_column]):
                     attachments_filenames.add(attachment_filename)
@@ -270,7 +304,7 @@ class Component(ComponentBase):
         message = VALID_PLAINTEXT_TEMPLATE_MESSAGE if plaintext else VALID_HTML_TEMPLATE_MESSAGE
         key_template_column = KEY_PLAINTEXT_TEMPLATE_COLUMN if plaintext else KEY_HTML_TEMPLATE_COLUMN
         message_type = MessageType.SUCCESS
-        template_column = self.cfg.message_body_config[key_template_column]
+        template_column = self.cfg.advanced_options.message_body_config[key_template_column]
         missing_columns = self._get_missing_columns_from_table(reader, template_column)
         if missing_columns:
             message = '❌ - Missing columns: ' + ', '.join(missing_columns)
@@ -279,7 +313,7 @@ class Component(ComponentBase):
 
     def _read_template_text(self, plaintext: bool = True) -> str:
         """Reads in template either from file, or from config"""
-        message_body_config = self.cfg.message_body_config
+        message_body_config = self.cfg.advanced_options.message_body_config
         message_body_source = message_body_config.message_body_source
 
         if message_body_source == 'from_template_file':
@@ -329,7 +363,7 @@ class Component(ComponentBase):
             reader = csv.DictReader(in_table)
             columns = set(reader.fieldnames)
 
-            if self.cfg.message_body_config.message_body_source == 'from_table':
+            if self.cfg.advanced_options.message_body_config.message_body_source == 'from_table':
                 return self._validate_templates_from_table(reader, plaintext)
 
         template_text = self._read_template_text(plaintext)
@@ -340,7 +374,7 @@ class Component(ComponentBase):
             return ValidationResult(str(e), MessageType.DANGER)
 
     def __exit__(self):
-        self._client._smtp_server.close()
+        self._client.smtp_server.close()
 
     def test_smtp_server_connection_(self) -> ValidationResult:
         connection_config = ConnectionConfig.load_from_dict(self.configuration.parameters['connection_config'])
@@ -355,7 +389,7 @@ class Component(ComponentBase):
         return self.test_smtp_server_connection_()
 
     def validate_subject_(self) -> ValidationResult:
-        subject_config = SubjectConfig.load_from_dict(self.configuration.parameters['subject_config'])
+        subject_config = SubjectConfig.load_from_dict(self.configuration.parameters.advanced_options['subject_config'])
         message = VALID_SUBJECT_MESSAGE
         subject_column = None
         if subject_config.subject_source == 'from_table':
@@ -399,7 +433,7 @@ class Component(ComponentBase):
     def validate_attachments_(self) -> ValidationResult:
         self._init_configuration()
         message = VALID_ATTACHMENTS_MESSAGE
-        if self.cfg.attachments_config.attachments_source != 'all_input_files':
+        if self.cfg.advanced_options.attachments_config.attachments_source != 'all_input_files':
             input_filenames = set([file['name'] for file in self._list_files_in_sync_actions()])
             in_table_path = self._download_table_from_storage_api()
             expected_input_filenames = self._get_attachments_filenames_from_table(in_table_path)
@@ -422,7 +456,7 @@ class Component(ComponentBase):
             self.validate_subject_,
             self.validate_plaintext_template_,
             self.validate_attachments_]
-        if self.cfg.message_body_config.use_html_template:
+        if self.cfg.advanced_options.message_body_config.use_html_template:
             validation_methods.insert(3, self.validate_html_template_)
 
         messages = [validation_method().message for validation_method in validation_methods]
