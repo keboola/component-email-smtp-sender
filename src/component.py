a74e324fa879e7f582bacb13484c0812d2a6f468
SgtMarmite <d.novotny31415@gmail.com>
2024-06-21 13:37:17 +0200

    raise proper Exception when no files found

--- src/component.py
+++ src/component.py
@@ -352,7 +352,8 @@ class Component(ComponentBase):
             template_filename = message_body_config[key_template_filename]
             files = self._list_files_in_sync_actions()
             if not files:
-                raise UserException('No files found in the storage')
+                raise UserException('No files found in the storage. Please use tags to select your files instead of '
+                                    'query.')
             template_file_id = next(file['id'] for file in files if file['name'] == template_filename)
             template_path = self._download_file_from_storage_api(template_file_id)
             template_text = self._read_template_file(template_path)
